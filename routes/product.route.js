c4f9eb52d2e123bd9b337f5e123969420ff3402d
Parag <prajhan1@binghamton.edu>
2018-12-08 16:31:52 -0500

    Update operation

--- routes/product.route.js
+++ routes/product.route.js
@@ -12,5 +12,7 @@ router.post('/create', product_controller.product_create);
 
 router.get('/:id', product_controller.product_details);
 
+router.put('/:id/update', product_controller.product_update);
+
 module.exports = router;
 
